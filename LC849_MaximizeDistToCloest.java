ac236fbc34db9481f6ed49b8f45d6a309f3e1e6d
ichbinas <37156739+ichbinas@users.noreply.github.com>
2018-11-09 09:19:32 -0800

    Create LC849_MaximizeDistToCloest.java

---
+++ LC849_MaximizeDistToCloest.java
@@ -0,0 +1,37 @@
+class LC849_MaximizeDistToCloest {
+    public int maxDistToClosest(int[] seats) {
+        //For each emptySlot window [x,y], calculate minDist
+        int left = 0;
+        int right = 0;
+        int globalLeft = -1;
+        int globalRight = -1;
+        int globalMaxDist = 0;
+        int maxIndex = seats.length - 1;
+        
+        while (left < seats.length && right < seats.length) {
+            while (left < seats.length && seats[left] == 1) left++;
+            right = left;
+            while (right+1 < seats.length && seats[right+1] == 0) right++;
+            
+            if (right < seats.length) {
+                int locDist = getDist(left, right, maxIndex);
+                if (locDist > globalMaxDist) {
+                    globalLeft = left;
+                    globalRight = right;
+                    globalMaxDist = locDist;
+                }
+            }
+            left = right + 1;
+        }
+        
+        return globalMaxDist;
+    }
+    
+    private int getDist(int left, int right, int maxIndex) {
+        if (left == 0 || right == maxIndex) {
+            return right - left + 1;
+        } else {
+            return ((right - left) >> 1) + 1;
+        }
+    }
+}
